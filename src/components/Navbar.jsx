b3c29381784911d6d7d8825278b765b75d5c41ee
pratsy91 <pratapsinghchouhan412@gmail.com>
2025-04-14 17:31:11 +0530

    fix: stripe message updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -130,7 +130,7 @@ export function Navbar() {
 
       {/* Stripe Message */}
       {showStripeMessage && (
-        <div className="fixed bottom-4 left-1/2 transform -translate-x-1/2 bg-blue-600 text-white px-6 py-3 rounded-md shadow-lg">
+        <div className="fixed bottom-4 left-1/2 transform -translate-x-1/2 bg-blue-600 text-white px-6 py-3 rounded-md shadow-lg z-20">
           <p className="text-lg font-semibold">
             Stripe Payment Integration is Live!
           </p>
